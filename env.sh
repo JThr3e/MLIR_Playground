206751733e89bbab52d5a946e57f5f2b1aa87fad
Joseph <j768miner@gmail.com>
2024-06-01 12:18:33 -0700

    standalone

---
+++ env.sh
@@ -0,0 +1,3 @@
+export BUILD_DIR=/home/jthr3e/Documents/Compilers/LLVM_playground/llvm-project/build/
+export PREFIX=/home/jthr3e/Documents/Compilers/LLVM_playground/llvm-project/build/
+export LLVM_SRC_DIR=/home/jthr3e/Documents/Compilers/LLVM_playground/llvm-project/
